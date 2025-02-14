62532fe15a75712d12e9bf26e3cfb72cd32b02ec
strayge <strayge@gmail.com>
2025-02-14 08:37:20 +0400

    reuse same logic for auto activation and venva cmd

--- conf.d/autovenv2.fish
+++ conf.d/autovenv2.fish
@@ -16,7 +16,7 @@ if status is-interactive
 end
 
 # Default activate.fish script with minor modifications
-# default Windows venv does not have an activate.fish script
+# required, as default Windows venv does not have an activate.fish script
 function activate -d "Activate a Python virtual environment"
     function deactivate -d "Exit virtual environment and return to normal shell environment"
         # reset old environment variables
@@ -99,6 +99,71 @@ function select
   echo $arr[$argv]
 end
 
+function __is_subdirectory --description "Check if one directory is parent of another (after link resolution)"
+    set -l parent (realpath "$argv[1]")
+    set -l child (realpath "$argv[2]")
+    string match -q -- "$parent/*" "$child"
+end
+
+function __is_valid_venv_directory --description "Check if the directory is a valid virtual environment"
+    set -l venv_dir $argv[1]
+    test -d "$venv_dir" -a -e "$venv_dir/bin/activate" -o -e "$_venv_dir/Scripts/activate"
+end
+
+function __check_venv_directory --description "Check if the directory has a virtual environment"
+    set -l directory $argv[1]
+
+    # is autovenv_file presence
+    if test -f "$directory/$autovenv_file"
+        set -l venv_dir (cat "$_tree/$autovenv_file" | string trim)
+        # is directory from file valid
+        if test -n "$venv_dir" -a -d "$venv_dir"
+            if __is_subdirectory "$autovenv_envs" "$venv_dir"
+                if __is_valid_venv_directory "$venv_dir"
+                    echo "$venv_dir"
+                    return
+                end
+            end
+        end
+    end
+
+    # is directory from autovenv_dirs presence
+    for subdirectory in $autovenv_dirs
+        set -l venv_dir "$directory/$subdirectory"
+        # skip if subdirectory is not present
+        if not test -d "$venv_dir"
+            continue
+        end
+        if not __is_subdirectory "$directory" "$venv_dir"
+            continue
+        end
+        if __is_valid_venv_directory "$venv_dir"
+            echo "$venv_dir"
+            return
+        end
+    end
+end
+
+# Function to find venv specific directory
+function __detect_venv_directory --description "Detects the virtual environment directory"
+    set -l initial_dir $argv[1]
+    set -l dir "$initial_dir/."
+
+    while true
+        set -l prev_dir "$dir"
+        set -l dir (path dirname -- "$dir")
+        if test -z "$dir" -o "$dir" = "$prev_dir"
+            break
+        end
+        set -l venv_dir (__check_venv_directory "$dir")
+        if test -n "$venv_dir"
+            echo "$venv_dir"
+            return
+        end
+    end
+end
+
+
 ## AutoVenv Function
 # Activates on directory changes.
 function autovenv --on-variable PWD -d "Automatic activation of Python virtual environments"
@@ -106,60 +171,17 @@ function autovenv --on-variable PWD -d "Automatic activation of Python virtual e
     test ! "$autovenv_enable" = "yes"
         or not status is-interactive
         and return
-    # Start at PWD (prsent working directory, see if there is a subfolder that contains bin/activate
-    # ie. start at $PWD/<subdir from autovenv_dirs>/bin/activate
-    # If that doesn't exist, try $PWD/../<subdir from autovenv_dirs>/bin/activate
-    # Keep going until we cannot go any further
-    set -l _tree "$PWD/."
-    set -l _done false
-    while true
-        set -l _tree_old "$_tree"
-        set _tree (path dirname -- "$_tree")
-        if test -z "$_tree" -o "$_tree" = "$_tree_old"
-            # dirname of / is /, so we've reached the end
-            break
+
+    set -l found_dir (__detect_venv_directory "$PWD")
+    if test -n "$found_dir"
+        set _source "$found_dir"
+        if test "$autovenv_announce" = "yes"
+            set -g __autovenv_old $__autovenv_new
+            set -g __autovenv_new (path basename -- "$found_dir")
+            set -l venv_dir "$found_dir"
         end
-
-        set -l _possible_dirs
-
-        # check autovenv_file presence
-        if test -f "$_tree/$autovenv_file"
-            set -l _venv_dir_from_file (cat "$_tree/$autovenv_file" | string trim)
-            if test -n "$_venv_dir_from_file" -a -d "$_venv_dir_from_file"
-                # add directory from autovenv_file to possible directories
-                set -a _possible_dirs "$_venv_dir_from_file"
-            end
-        end
-
-        # check directory from autovenv_dirs presence
-        for _venv_dir in (path filter -d -- "$_tree"/*)
-            set -l _venv_dir_basename (path basename -- "$_venv_dir")
-            for _dir in $autovenv_dirs
-                if string match -q -- "$_dir" "$_venv_dir_basename"
-                    set -a _possible_dirs "$_venv_dir"
-                end
-            end
-        end
-
-        for _dir in $_possible_dirs
-            # found a match, check for the presence of the activate script
-            if test -e "$_dir/bin/activate" -o -e "$_dir/Scripts/activate"
-                set _source "$_dir"
-                if test "$autovenv_announce" = "yes"
-                    set -g __autovenv_old $__autovenv_new
-                    set -g __autovenv_new (path basename -- "$_dir")
-                    set venv_dir $_dir
-                end
-                set _done true
-                break
-            end
-        end
-
-        if $_done
-            break
-        end
-
     end
+
     # If we're *not* in an active venv and the venv source dir exists we activate it and return.
     if test -z "$VIRTUAL_ENV" -a -e "$_source"
         activate "$_source"
@@ -169,7 +191,7 @@ function autovenv --on-variable PWD -d "Automatic activation of Python virtual e
     # Next we check to see if we're already in an active venv. If so we proceed with further tests.
     else if test -n "$VIRTUAL_ENV"
         # Check to see if our CWD is inside the venv directory.
-        set _dir (string match -n "$VIRTUAL_ENV*" "$venv_dir")
+        set -l _dir (string match -n "$VIRTUAL_ENV*" "$venv_dir")
         # If we're no longer inside the venv dirctory deactivate it and return.
         if test -z "$_dir" -a ! -e "$_source"
             deactivate
@@ -193,6 +215,11 @@ end
 ## Extra functions for external venv management
 
 function venvls -d "List external virtual environments"
+    if contains -- '-h' $argv
+        echo "Usage: venvls"
+        echo "List external virtual environments."
+        return
+    end
     set -l _dirs (path filter -d -- "$autovenv_envs"/*)
     for _venv in $_dirs
         echo (path basename -- "$_venv")
@@ -200,31 +227,56 @@ function venvls -d "List external virtual environments"
 end
 
 function venva -d "Activate an external virtual environment"
-    set -l _name
-    if test -z "$argv"
-        set _name (path basename -- "$PWD")
-    else
-        set _name "$argv[1]"
-    end
-
-    set -l _venv_dir (path normalize "$autovenv_envs/$_name")
-    # Normalized path must be inside the autovenv_envs directory
-    set -l _match (string match -- "$autovenv_envs/*" "$_venv_dir")
-    if test ! "$_match"
-        echo "Invalid virtual environment."
+    if contains -- '-h' $argv
+        echo "Usage: venva [name]"
+        echo "Activate an external virtual environment."
+        echo "If explicit name is not provided, $autovenv_file file or current directory name is used."
         return
     end
 
-    if ! test -d "$_venv_dir"
-        echo "Virtual environment does not exist."
+    set -l name "$argv[1]"
+    set -l directory (path dirname -- "$PWD")
+
+    set -f venv_dir
+
+    if test -n "$name"
+        set -l test_dir (path normalize "$autovenv_envs/$name")
+        if ! __is_valid_venv_directory "$test_dir"
+            echo "Virtual environment does not exist."
+            return
+        end
+        set -f venv_dir "$test_dir"
+    end
+
+    if test -z "$venv_dir"
+        set -f venv_dir (__detect_venv_directory "$PWD")
+    end
+
+    if test -z "$venv_dir"
+        set -l test_dir (path normalize "$autovenv_envs/$directory")
+        if __is_valid_venv_directory "$test_dir"
+            set -f venv_dir "$test_dir"
+        end
+    end
+
+    if test -z "$venv_dir"
+        echo "Virtual environment not found."
         return
     end
 
-    activate "$_venv_dir"
+    activate "$venv_dir"
     echo "Virtual environment activated."
 end
 
 function venvmk -d "Create a new external virtual environment"
+    if contains -- '-h' $argv
+        echo "Usage: venvmk [-p python] [-m] [name]"
+        echo "Create a new external virtual environment."
+        echo "If no name is provided, the current directory name is used."
+        echo "  -p python: Specify the Python interpreter to use."
+        echo "  -m: Manual mode, do not create $autovenv_file file."
+        return
+    end
     argparse 'p/python=' 'm/manual' -- $argv
     if test $status -ne 0
         return
@@ -275,6 +327,12 @@ function venvmk -d "Create a new external virtual environment"
 end
 
 function venvrm -d "Remove an external virtual environment"
+    if contains -- '-h' $argv
+        echo "Usage: venvrm [name]"
+        echo "Remove an external virtual environment."
+        echo "If no name is provided, the current directory name is used."
+        return
+    end
     set -l _name
     if test -z "$argv"
         set _name (path basename -- "$PWD")
